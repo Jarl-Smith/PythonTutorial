7c7318183377fb826eb6c25b80c7b73b65d1c2d2
Jarl-Smith <1994636224@qq.com>
2017-12-22 09:07:05 +0800

    字符串知识

--- MyEncrypt/test.py
+++ MyEncrypt/test.py
@@ -3,19 +3,20 @@
 # 解密：以分隔符来拆解密文保存到列表里，将最后一个空字符剔除,将ASCII码转换为字符保存到需要显示的变量中
 
 message = 'The Elder Scroll V Skyrim'  # 明文
-store = ''  # 需要显示的变量
+store = []  # 需要显示的变量
 splitter = '%%'  # 分隔符
 
 # 加密过程
 if False:
 	for c in message:
-		store = store + str(ord(c)) + splitter
+		store.append(str(ord(c)))  # 将明文的单个字符转换为ASCII码存储到列表中
+	temp = splitter.join(store)  # 相邻两个元素添加分隔符保存到打印变量中
 	try:
 		with open('demo.txt', 'w') as file1:
-			file1.write(store)
+			file1.write(temp)  # 将打印变量输出到文件中
+		print('storage success')
 	except IOError as ioe:
 		print(str(ioe))
-
 # 解密过程
 else:
 	try:
@@ -23,7 +24,7 @@ else:
 			for line in file1:
 				list_temp = line.split(splitter)  # 最后的空字符会保存到列表里
 				for c in list_temp:
-					store = store + chr(int(c))  # 转换空字符会报错ValueError
+					store.append(chr(int(c)))  # 转换空字符会报错ValueError
 	except IOError as ioe:
 		print(str(ioe))
 	except ValueError:
