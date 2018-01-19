c836db51bb382759fa698ee0034c31733d48d58f
1994636224@qq.com <123456qwe`>
2018-01-19 11:41:23 +0800

    修改注释方式

--- MyEncrypt/test.py
+++ MyEncrypt/test.py
@@ -1,6 +1,8 @@
-# 自定义加密解密程序
-# 加密：将明文每个字符的ASCII码存储，每个字符之间以自定义分隔符来分割
-# 解密：以分隔符来拆解密文保存到列表里，将最后一个空字符剔除,将ASCII码转换为字符保存到需要显示的变量中
+"""
+自定义加密解密程序
+加密：将明文每个字符的ASCII码存储，每个字符之间以自定义分隔符来分割
+解密：以分隔符来拆解密文保存到列表里，将最后一个空字符剔除,将ASCII码转换为字符保存到需要显示的变量中
+"""
 
 message = 'The Elder Scroll V Skyrim'  # 明文
 store = []  # 需要显示的变量
@@ -8,26 +10,26 @@ splitter = '%%'  # 分隔符
 
 # 加密过程
 if False:
-	for c in message:
-		store.append(str(ord(c)))  # 将明文的单个字符转换为ASCII码存储到列表中
-	temp = splitter.join(store)  # 相邻两个元素添加分隔符保存到打印变量中
-	try:
-		with open('demo.txt', 'w') as file1:
-			file1.write(temp)  # 将打印变量输出到文件中
-		print('storage success')
-	except IOError as ioe:
-		print(str(ioe))
+    for c in message:
+        store.append(str(ord(c)))  # 将明文的单个字符转换为ASCII码存储到列表中
+    temp = splitter.join(store)  # 相邻两个元素添加分隔符保存到打印变量中
+    try:
+        with open('demo.txt', 'w') as file1:
+            file1.write(temp)  # 将打印变量输出到文件中
+        print('storage success')
+    except IOError as ioe:
+        print(str(ioe))
 # 解密过程
 else:
-	try:
-		with open('demo.txt', 'r') as file1:
-			for line in file1:
-				list_temp = line.split(splitter)  # 最后的空字符会保存到列表里
-				for c in list_temp:
-					store.append(chr(int(c)))  # 转换空字符会报错ValueError
-	except IOError as ioe:
-		print(str(ioe))
-	except ValueError:
-		pass
-	finally:
-		print(store)
+    try:
+        with open('demo.txt', 'r') as file1:
+            for line in file1:
+                list_temp = line.split(splitter)  # 最后的空字符会保存到列表里
+                for c in list_temp:
+                    store.append(chr(int(c)))  # 转换空字符会报错ValueError
+    except IOError as ioe:
+        print(str(ioe))
+    except ValueError:
+        pass
+    finally:
+        print(store)
--- PickleTutorial/main.py
+++ PickleTutorial/main.py
@@ -1,17 +1,19 @@
-import pickle
+"""
+数据腌制，类似Java的序列化，文件访问模式只能是二进制模式读/写
+Python提供了一个标准库，名为pickle，他可以保存和加载几乎任何Python数据对象，包括列表
+该模块是读取数据
+"""
 
-# 数据腌制，类似Java的序列化，文件访问模式只能是二进制模式读/写
-# Python提供了一个标准库，名为pickle，他可以保存和加载几乎任何Python数据对象，包括列表
-# 该模块是读取数据
+import pickle
 
 message = []
 
 try:
-	with open('asd.txt', 'rb') as file:
-		message = pickle.load(file)
+    with open('asd.txt', 'rb') as file:
+        message = pickle.load(file)
 except IOError:
-	print(str(IOError))
+    print(str(IOError))
 except pickle.PickleError:
-	print(str(pickle.PickleError))
+    print(str(pickle.PickleError))
 
 print(message)
